c44b9996a05d8fa88e4ef3fba4bf6d5ec2766fd0
Jean-Pierre Bianchi <jpbianchi@gmail.com>
2023-08-20 23:26:41 +0200

    Podcast debug

--- PODCAST/1/podcast_frontend.py
+++ PODCAST/1/podcast_frontend.py
@@ -121,7 +121,7 @@ def main():
     
     st.title("🎙️ Week's Pod Spark!")
     
-    available_podcast_info = create_dict_from_json_files('PODCAST/1')
+    available_podcast_info = create_dict_from_json_files('.')
 
     # Left section - Input fields
     st.sidebar.header("Podcast RSS Feeds")
