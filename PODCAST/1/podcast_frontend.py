890732544123556acb5d64bce1e6f4828ffb9c54
Jean-Pierre Bianchi <jpbianchi@gmail.com>
2023-08-20 22:13:00 +0200

    Podcast using currrent dir

--- PODCAST/1/podcast_frontend.py
+++ PODCAST/1/podcast_frontend.py
@@ -5,6 +5,7 @@ import os
 
 def main():
 
+    curdir = os.getcwd()
     # Inject custom CSS to set the background color
     st.markdown(
         """
@@ -99,12 +100,12 @@ def main():
         """,
         unsafe_allow_html=True,
     )
-    st.image('PODCAST/1/bg-image.jpg', use_column_width=True)
+    st.image(f'{curdir}/bg-image.jpg', use_column_width=True)
     
     st.title("🎙️ Week's Pod Spark!")
     
     
-    available_podcast_info = create_dict_from_json_files('PODCAST/1/')
+    available_podcast_info = create_dict_from_json_files(curdir)
 
     # Left section - Input fields
     st.sidebar.header("Podcast RSS Feeds")
